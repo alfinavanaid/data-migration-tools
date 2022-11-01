8d0f940a3e6a950cef76e9282ea9bd27b1fefde1
Mochammad Alfin Faturochman <82384511+alfinfaturochmanm@users.noreply.github.com>
2022-11-01 09:12:27 +0700

    Data Extract : Create Controller

---
+++ src/Controller/ExtractController.php
@@ -0,0 +1,25 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Lib\HandsomeAlfin\DataMigrationTools\DataMigrationTools;
+
+class ExtractController
+{
+
+    public function index() : Response
+    {
+        $data_source_json = file_get_contents('http://127.0.0.1:8000/storage/data_source_object.json');
+        $DataMigrationTools = new DataMigrationTools();
+        $DataMigrationTools->data_source_json = $data_source_json;
+        $DataMigrationTools->extract();
+        // $DataMigrationTools->analyze();
+        
+        // file_put_contents('storage/table_not_found.json', json_encode($DataMigrationTools->table_not_found), 1);
+        // file_put_contents('storage/data_source_object.json', json_encode($DataMigrationTools->data_source_json), 1);
+
+        return new Response('oke', 200);
+    }
+}
